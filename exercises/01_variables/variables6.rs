f9c0426dcc667487a04d18685e7c6b6fce315015
LeIGrec <122344955+LeIGrec@users.noreply.github.com>
2024-04-26 08:46:51 +0200

    Update variables6.rs

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -6,8 +6,11 @@
 /*
 WRITE UP:
 
-En Rust lorsqu'on déclare une constante avec le mot clé const, 
-on doit spécifier le type de la constante. Le compilateur Rust doit connaitre le type de chaque variable au moment de la compilation.
+Dans ce programme Rust, nous déclarons une constante nommée NUMBER de type i32 (entier signé 32 bits) et lui attribuons la valeur 3.
+
+La déclaration de constante utilise le mot-clé const suivi du nom de la constante, des deux-points et du type de données. Ensuite, le signe égal (=) est utilisé pour assigner une valeur à la constante.
+
+Dans la fonction main(), nous utilisons la macro println! pour afficher la valeur de la constante NUMBER en utilisant le marqueur de format {}.
 
 */
 
